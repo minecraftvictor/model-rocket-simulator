f004e7010a95e1f2193080e801f8dc435adaff5d
21ttran11 <58841978+21ttran11@users.noreply.github.com>
2025-10-29 17:04:19 -0700

    Revert "Merge branch 'rocketlaunch'"
    This reverts commit 15fd8941a3f0cb300e31d6245345a48d451a55dc, reversing
    changes made to 0a0470a2c000aaab3db87c25c526a96288a2d4ae.

--- Assets/scripts/RocketScript.cs
+++ Assets/scripts/RocketScript.cs
@@ -5,7 +5,6 @@ public class RocketScript : MonoBehaviour
 {
    public Rigidbody rb;
 
-
     public Motor motor; 
 
     public float motorForce;
@@ -13,31 +12,15 @@ public class RocketScript : MonoBehaviour
     public float distance;
     public Vector3 startPosition;
 
-    public bool launched = false;
-    public bool flipped = false;
-    public float minUpSpeed = 1f;
     void Start()
     {
       rb = GetComponent<Rigidbody>();
-      launched = false;
     }
 
     void Update()
     {
       distance = Vector3.Distance(startPosition, transform.position);
-    }
-
-    void FixedUpdate()
-    {
-        if (launched == true)
-        {
-          if (rb.linearVelocity.y < 0 && flipped == false)
-          {
-            Debug.Log("Falling");
-            rb.AddTorque(transform.right * 55f, ForceMode.Force);
-            flipped = true;
-          }
-        }
+      Debug.Log(distance);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void SelectMotor(Motor selectMotor)
@@ -51,7 +34,8 @@ public class RocketScript : MonoBehaviour
     public void LaunchRocket()
     {
       rb.AddForce(transform.up * motorForce, ForceMode.Impulse);
-      launched = true;
     }
+
+
 
 }
